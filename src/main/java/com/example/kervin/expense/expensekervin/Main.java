0392244907e55de0b85fe3b4bc1ef5bfbf53b106
kervinbalibagoso <kervinbalibagoso@gmail.com>
2020-08-16 19:20:22 +0800

    initial commit

---
+++ src/main/java/com/example/kervin/expense/expensekervin/Main.java
@@ -0,0 +1,13 @@
+package com.example.kervin.expense.expensekervin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kervin/expense/expensekervin/ExpenseKervinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kervin.expense.expensekervin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseKervinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
